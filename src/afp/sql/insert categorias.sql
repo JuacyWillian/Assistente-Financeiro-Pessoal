c0ae64881c328920491c144b62ecbddf2669bc1d
jw <jw@juacy-xub1604>
2016-05-20 12:08:37 -0300

    quase lancando a versão 0.0.1b só falta criar a edição e exclusão de categoria!

--- src/afp/sql/insert categorias.sql
+++ src/afp/sql/insert categorias.sql
@@ -4,3 +4,11 @@ INSERT INTO categorias(titulo, descricao) VALUES ("Serviços", "Despesas com ser
 INSERT INTO categorias(titulo, descricao) VALUES ("Entretenimento", "Despesas com entretenimento.");
 INSERT INTO categorias(titulo, descricao) VALUES ("Transportes", "Despesas com transportes");
 INSERT INTO categorias(titulo, descricao) VALUES ("Viagens", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("Salario", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("investimento", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("bonus salarial", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("Viagens", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("Viagens", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("Viagens", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("Viagens", "Despesas com viagens");
+INSERT INTO categorias(titulo, descricao) VALUES ("Viagens", "Despesas com viagens");
--- src/afp/sql/insert contas.sql
+++ src/afp/sql/insert contas.sql
@@ -1,2 +1,60 @@
 -- insert contas
-INSERT INTO contas(titulo, descricao, cat_id, tipo, valor, dt_criacao, dt_vencimento, quitado) VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO contas(titulo, descricao, cat_id, tipo, valor, dt_criacao, dt_vencimento, quitado) 
+VALUES (
+	"Conta de Luz de Abril", 
+    "Conta de luz do mes de abril", 
+    2, 
+    "DESPESA",
+    150,
+    20160401, 
+    20160405, 
+    true
+);
+
+INSERT INTO contas(titulo, descricao, cat_id, tipo, valor, dt_criacao, dt_vencimento, quitado) 
+VALUES (
+	"Conta de Luz de Maio", 
+    "Conta de luz do mes de maio", 
+    2, 
+    "DESPESA",
+    155,
+    20160501, 
+    20160505, 
+    false
+);
+
+INSERT INTO contas(titulo, descricao, cat_id, tipo, valor, dt_criacao, dt_vencimento, quitado) 
+VALUES (
+	"Conta de Gas de Abril", 
+    "Conta de Gas do mes de abril", 
+    2, 
+    "DESPESA",
+    100,
+    20160401, 
+    20160405, 
+    false
+);
+
+INSERT INTO contas(titulo, descricao, cat_id, tipo, valor, dt_criacao, dt_vencimento, quitado) 
+VALUES (
+	"Filme do Homem de Ferro 3", 
+    "Conta do cinema", 
+    3, 
+    "DESPESA",
+    30,
+    20160401, 
+    20160401, 
+    true
+);
+
+INSERT INTO contas(titulo, descricao, cat_id, tipo, valor, dt_criacao, dt_vencimento, quitado) 
+VALUES (
+	"Viagem para Maui",
+    "Viagem de férias para o hawai",
+    5,
+    "DESPESA",
+    3000,
+    20160401,
+    20160401,
+    true
+);

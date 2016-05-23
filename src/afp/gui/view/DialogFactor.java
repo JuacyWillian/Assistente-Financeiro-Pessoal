6f3272ed649712347db0974d36e770fcac9bff52
jw <jw@juacy-xub1604>
2016-05-23 20:31:56 -0300

    removendo mais arquivos desnecessários!

--- src/afp/gui/view/DialogFactor.java
+++ src/afp/gui/view/DialogFactor.java
@@ -32,7 +32,7 @@ import javafx.scene.layout.GridPane;
 public class DialogFactor {
 
     private static final String DEVELOPPERS = "Juacy Willian, Adolpho Santos, Rafael Oliveira";
-    private static final String VERSION = "0.0.01b";
+    private static final String VERSION = "1.0b";
     private static final String LICENCA = "GNU General Public License v3";
 
     /**
